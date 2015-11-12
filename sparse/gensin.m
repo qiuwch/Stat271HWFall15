802e8bfc95264741cecd2b6891d4ec4e6ad399d7
qiuwch <qiuwch@users.noreply.github.com>
2015-11-12 14:03:35 -0800

    change omega to omeganorm.

--- sparse/gensin.m
+++ sparse/gensin.m
@@ -2,12 +2,12 @@
 A=1;
 sz=[16 16];
 numimg=1e6;
-omega=1;% high frequency for 1e3, low frequency for 1
+omeganorm=1;% high frequency for 1e3, low frequency for 1
 patchsin=zeros(sz(1)*sz(2),numimg);
 
 for ii=1:numimg
     theta = pi*rand;
-    omega =omega* [cos(theta),sin(theta)]*(0.4*rand+0.8);%
+    omega =omeganorm* [cos(theta),sin(theta)]*(0.4*rand+0.8);%
     % omenorm*(0.8:1.2)
     rho=pi/2*rand;
     im=GenSinusoid(sz, A, omega, rho);
